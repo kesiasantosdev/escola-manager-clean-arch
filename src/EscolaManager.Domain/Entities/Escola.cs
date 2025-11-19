496e4a348a75a101800e015ddaa17e1d0df117c7
Kesia <kesiasantos.dev@gmail.com>
2025-11-19 11:29:51 -0300

    feat: criar entidade Escola com modelo rico e remover Class1

---
+++ src/EscolaManager.Domain/Entities/Escola.cs
@@ -0,0 +1,33 @@
+﻿namespace EscolaManager.Domain.Entities
+{
+    public class Escola
+    {
+        public Guid Id { get; private set; }
+        public string NomeEscola { get; private set; }
+        public string Cnpj { get; private set; }
+
+        public Escola(string nomeEscola, string cnpj)
+        {
+            if (string.IsNullOrWhiteSpace(nomeEscola))
+                throw new ArgumentException("O nome da Escola é obrigatório.", nameof(nomeEscola));
+
+            Id = Guid.NewGuid();
+            NomeEscola = nomeEscola;
+            Cnpj = cnpj;
+        }
+
+        public void AlterarNome(string novoNome)
+        {
+            if (string.IsNullOrWhiteSpace(novoNome))
+                throw new ArgumentException("O nome não pode ser vazio", nameof(novoNome));
+
+            NomeEscola = novoNome;
+        }
+
+        protected Escola()
+        {
+            NomeEscola = string.Empty;
+            Cnpj = string.Empty;
+        }
+    }
+}
