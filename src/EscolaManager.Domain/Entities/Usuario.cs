4a4f5be5ba3b4acd06e4de32a8baf4e263fcfec0
Kesia <kesiasantos.dev@gmail.com>
2025-11-19 14:45:04 -0300

    feat: criar entidade Usuario com relacionamentos e hierarquia

---
+++ src/EscolaManager.Domain/Entities/Usuario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaManager.Domain.Entities
+{
+    public class Usuario
+    {
+        public Guid Id { get; private set; }
+        public Guid PessoaId { get; private set; }
+        public virtual Pessoa? Pessoa { get; private set; }
+        public Guid EscolaId { get; private set; }
+        public virtual Escola? Escola { get; private set; }
+        public Guid CargoId { get; private set; }
+        public virtual Cargo? Cargo { get; private set; }
+        public Guid? SuperiorId { get; private set; }
+        public virtual Usuario? Superior { get; private set; }
+
+        public Usuario(Guid pessoaId, Guid escolaId, Guid cargoId)
+        {
+            if (pessoaId == Guid.Empty) throw new ArgumentException("Usuario deve estar vinculado a uma Pessoa.", nameof(pessoaId));
+            if (escolaId == Guid.Empty) throw new ArgumentException("Usuario deve estar vinculado a uma Escola.", nameof(escolaId));
+            if (cargoId == Guid.Empty) throw new ArgumentException("Usuario deve ter um Cargo.", nameof(cargoId));
+
+            Id = Guid.NewGuid();
+            PessoaId = pessoaId;
+            EscolaId = escolaId;
+            CargoId = cargoId;
+            SuperiorId = null;
+        }
+
+        public void DefinirSuperior(Guid? superiorId)
+        {
+            if (superiorId.HasValue && superiorId == Id)
+                throw new InvalidOperationException("Um usuário não pode ser superior de si mesmo.");
+
+            SuperiorId = superiorId;
+        }
+
+        public void AlterarCargo(Guid novoCargoId)
+        {
+            if (novoCargoId == Guid.Empty)
+                throw new ArgumentException("O novo cargo deve ser válido.", nameof(novoCargoId));
+
+            CargoId = novoCargoId;
+        }
+
+        protected Usuario()
+        {
+
+        }
+    }
+
+}
