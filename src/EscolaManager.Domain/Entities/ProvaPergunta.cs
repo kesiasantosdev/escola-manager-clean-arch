a337d74f8db0e083c4f30f2f036f665c894cc3ee
Kesia <kesiasantos.dev@gmail.com>
2025-11-21 09:35:37 -0300

    feat: criar entidade Prova e entidade de ligacao ProvaPergunta

---
+++ src/EscolaManager.Domain/Entities/ProvaPergunta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaManager.Domain.Entities
+{
+    public class ProvaPergunta
+    {
+        public Guid ProvaId { get; private set; }
+        public virtual Prova? Prova { get; private set; }
+        public Guid PerguntaId { get; private set; }
+        public virtual Pergunta? Pergunta { get; private set; }
+        public int Ordem {  get; private set; }
+
+        public ProvaPergunta(Guid provaId, Guid perguntaId, int ordem)
+        {
+            if (provaId == Guid.Empty) throw new ArgumentException("Prova inválida.", nameof(provaId));
+            if (perguntaId == Guid.Empty) throw new ArgumentException("Pergunta inválida.", nameof(perguntaId));
+            if (ordem <= 0) throw new ArgumentException("A ordem deve ser maior que zero.", nameof(ordem));
+
+            ProvaId = provaId;
+            PerguntaId = perguntaId;
+            Ordem = ordem;
+        }
+
+        protected ProvaPergunta() { }
+
+    }
+}
