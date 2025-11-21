f1a56c86a4beb1c2509b370a272d5025bbb1f359
Kesia <kesiasantos.dev@gmail.com>
2025-11-21 15:00:48 -0300

    feat: implementar EscolaDbContext e configuracao de tabela Cargo

---
+++ src/EscolaManager.Infrastructure/Data/EscolaDbContext.cs
@@ -0,0 +1,38 @@
+﻿using EscolaManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaManager.Infrastructure.Data
+{
+    public class EscolaDbContext : DbContext
+    {
+        public EscolaDbContext(DbContextOptions<EscolaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Escola> Escolas { get; set; }
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Permissao> Permissoes { get; set; }
+        public DbSet<TipoPergunta> TiposPerguntas { get; set; }
+        public DbSet<TipoResposta> TiposRespostas { get;set; }
+        public DbSet<Pergunta> Perguntas { get; set; }
+        public DbSet<Prova> Provas {  get; set; }
+        public DbSet<Bimestre> Bimestres { get; set; }
+        public DbSet<RealizacaoProva> RealizacoesProvas { get; set; }
+        public DbSet<Resposta> Respostas { get;set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
