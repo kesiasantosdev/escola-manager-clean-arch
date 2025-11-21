5fe8c60e3b335501d0f754266825fe023609f1b0
Kesia <kesiasantos.dev@gmail.com>
2025-11-21 08:56:51 -0300

    feat: criar entidade TipoResposta com validacoes

---
+++ src/EscolaManager.Domain/Entities/TipoResposta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaManager.Domain.Entities
+{
+    internal class TipoResposta
+    {
+    }
+}
