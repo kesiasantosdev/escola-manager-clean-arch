ba8d71d2e4c9e426bfd2bf432806c045fdfa5eb0
Kesia <kesiasantos.dev@gmail.com>
2025-11-24 11:49:04 -0300

    feat: criar primeira migration

---
+++ src/EscolaManager.Infrastructure/Data/Configurations/ProvaPerguntaConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using EscolaManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EscolaManager.Infrastructure.Data.Configurations
+{
+    public class ProvaPerguntaConfiguration : IEntityTypeConfiguration<ProvaPergunta>
+    {
+        public void Configure(EntityTypeBuilder<ProvaPergunta> builder)
+        {
+            builder.ToTable("ProvaPerguntas");
+
+            // CHAVE COMPOSTA
+            builder.HasKey(pp => new { pp.ProvaId, pp.PerguntaId });
+
+            // RELACIONAMENTO COM PROVA
+            builder.HasOne(pp => pp.Prova)
+                   .WithMany(p => p.Questoes)
+                   .HasForeignKey(pp => pp.ProvaId);
+
+            // RELACIONAMENTO COM PERGUNTA
+            builder.HasOne(pp => pp.Pergunta)
+                   .WithMany() // se quiser criar coleção do outro lado, mude aqui
+                   .HasForeignKey(pp => pp.PerguntaId);
+
+            builder.Property(pp => pp.Ordem)
+                   .IsRequired();
+        }
+    }
+}
---
+++ src/EscolaManager.Infrastructure/Data/EscolaDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EscolaManager.Infrastructure.Data
+{
+    public class EscolaDbContextFactory : IDesignTimeDbContextFactory<EscolaDbContext>
+    {
+        public EscolaDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EscolaDbContext>();
+
+            // Configuração direta para a Migration funcionar sem depender da API
+            optionsBuilder.UseSqlServer("Server=localhost;Database=EscolaManagerDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new EscolaDbContext(optionsBuilder.Options);
+        }
+    }
+}
