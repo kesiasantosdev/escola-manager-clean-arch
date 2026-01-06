1194f41cb998dcc73e5e71ca7714284454c5f8e0
Kesia <kesiasantos.dev@gmail.com>
2026-01-06 12:50:59 -0300

    feat(cargo):  implementa novo método no repositório

--- src/EscolaManager.Infrastructure/Repositories/CargoRepository.cs
+++ src/EscolaManager.Infrastructure/Repositories/CargoRepository.cs
@@ -1,11 +1,20 @@
 ﻿using EscolaManager.Domain.Entities;
 using EscolaManager.Domain.Interfaces;
 using EscolaManager.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace EscolaManager.Infrastructure.Repositories
 {
     public class CargoRepository : Repository<Cargo>, ICargoRepository
     {
         public CargoRepository(EscolaDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<Cargo>> ObterPorEscolaAsync(Guid escolaId)
+        {
+            return await _context.Cargos
+                                 .AsNoTracking()
+                                 .Where(c => c.EscolaId == escolaId)
+                                 .ToListAsync();
+        }
     }
 }
